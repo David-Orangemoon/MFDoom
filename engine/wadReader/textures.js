5b0b66ee0272a3f71020ea8aee3cb14e812a1428
Obvious Alex C <76855369+David-Orangemoon@users.noreply.github.com>
2024-10-07 13:54:28 -0400

    ff

--- engine/wadReader/textures.js
+++ engine/wadReader/textures.js
@@ -51,7 +51,6 @@ window.textures = {
             textureIndexes.splice(0,headerEnd);
 
             //This shit barely works
-            console.log(textureName);
             for (let Y = 0; Y < texture.height; Y++) {
                 for (let X = 0; X < texture.width; X++) {
                     //R=0 G=1 B=2 A=3
